33e3628dcb7274fc16038c95ba103a35e96ca7bf
Quan Ngo <ngotrungquan1412@gmail.com>
2025-05-08 15:42:08 +0700

    FormatCode

--- src/pages/DiscountPage/index.js
+++ src/pages/DiscountPage/index.js
@@ -1,90 +1,88 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import Voucher_Item from '~/components/Voucher_Item';
-import * as PromotionsServices from '~/apiServices/promotionServices';
-import * as PromotionsCartServices from '~/apiServices/promotionCartServices';
-import ModalLoading from '~/components/ModalLoading';
-import { ToastContext } from '~/components/ToastContext';
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Voucher_Item from "~/components/Voucher_Item";
+import * as PromotionsServices from "~/apiServices/promotionServices";
+import * as PromotionsCartServices from "~/apiServices/promotionCartServices";
+import ModalLoading from "~/components/ModalLoading";
+import { ToastContext } from "~/components/ToastContext";
 function DiscountPage() {
-    const navigate = useNavigate();
-    const toastContext = useContext(ToastContext);
+  const navigate = useNavigate();
+  const toastContext = useContext(ToastContext);
 
+  const [loading, setLoading] = useState(false);
+  const [obj, setObj] = useState([]);
+  const [updatePage, setUpdatePage] = useState(new Date());
 
-    const [loading, setLoading] = useState(false);
-    const [obj, setObj] = useState(null);
-    const [updatePage, setUpdatePage] = useState(new Date());
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const fetchApi = async () => {
+      if (window.localStorage.getItem("UserLogin") === "false") {
+        toastContext.notify("info", "Bạn chưa đăng nhập");
+        navigate("/login");
+      } else {
+        setLoading(true);
+        setUser(JSON.parse(window.localStorage.getItem("user")));
+        const result = await PromotionsServices.getPromotionUser(
+          JSON.parse(window.localStorage.getItem("user"))._id
+        ).catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
 
-    const [user, setUser] = useState()
-    useEffect(() => {
-        const fetchApi = async () => {
-            if (window.localStorage.getItem("UserLogin") === 'false') {
-                toastContext.notify('info', 'Bạn chưa đăng nhập');
-                navigate('/login')
-            }
-            else {
-                setLoading(true)
-                setUser(JSON.parse(window.localStorage.getItem('user')))
-                const result = await PromotionsServices.getPromotionUser(JSON.parse(window.localStorage.getItem('user'))._id)
-                    .catch((err) => {
-                        console.log(err);
-                        setLoading(false)
-                    });
-
-                if (result) {
-                    console.log(result);
-                    setObj(result.data);
-                    setLoading(false)
-                }
-            }
-
+        if (result) {
+          console.log(result);
+          setObj(result.data);
+          setLoading(false);
         }
+      }
+    };
 
-        fetchApi();
+    fetchApi();
 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [updatePage]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [updatePage]);
 
-    const addPromotionCart = async (item) => {
-        const promotionCart = {
-            user: user,
-            discount: item
-        }
-        const fetchApi = async () => {
-            const result = await PromotionsCartServices.CreatePromotionCart(promotionCart)
-                .catch((err) => {
-                    console.log(err);
-                });
+  const addPromotionCart = async (item) => {
+    const promotionCart = {
+      user: user,
+      discount: item,
+    };
+    const fetchApi = async () => {
+      const result = await PromotionsCartServices.CreatePromotionCart(
+        promotionCart
+      ).catch((err) => {
+        console.log(err);
+      });
 
-            if (result) {
-                console.log(result);
-            }
+      if (result) {
+        console.log(result);
+      }
+    };
 
-        }
-
-        fetchApi();
-
-    }
-    return (
-        <div className='m-5 mb-10 p-3 rounded-lg min-h-[600px]'>
-            {
-                obj === null ? (<div><ModalLoading open={true} title={'Đang tải'} /></div>)
-                    :
-                    (
-                        <div className='mb-4 font-bold text-[18px]'>
-                            Khuyến mãi hôm nay
-                            <div className='flex flex-wrap gap-[5%] mt-4 justify-center md:justify-start'>
-                                {
-                                    obj.map((item, index) => (
-                                        <Voucher_Item discount={item} key={index} addToCart={addPromotionCart} />
-                                    ))
-                                }
-                            </div>
-
-                        </div>
-                    )}
-
+    fetchApi();
+  };
+  return (
+    <div className="m-5 mb-10 p-3 rounded-lg min-h-[600px]">
+      {obj === null ? (
+        <div>
+          <ModalLoading open={true} title={"Đang tải"} />
         </div>
-    );
+      ) : (
+        <div className="mb-4 font-bold text-[18px]">
+          Khuyến mãi hôm nay
+          <div className="flex flex-wrap gap-[5%] mt-4 justify-center md:justify-start">
+            {obj.map((item, index) => (
+              <Voucher_Item
+                discount={item}
+                key={index}
+                addToCart={addPromotionCart}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default DiscountPage;
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,154 +1,152 @@
-import React, { useState, useCallback, useContext, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Categories from '~/components/Categories';
-import ProductCarousel from '~/components/ProductCarousel';
-import Product_WC_item from '~/components/Product_WC_Item';
-import * as ProductServices from '~/apiServices/productServices'
-import * as CategoryServices from '~/apiServices/categoryServices';
-import * as ReportServices from '~/apiServices/reportServices'
-import { ToastContext } from '~/components/ToastContext';
+import React, { useState, useCallback, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Categories from "~/components/Categories";
+import ProductCarousel from "~/components/ProductCarousel";
+import Product_WC_item from "~/components/Product_WC_Item";
+import * as ProductServices from "~/apiServices/productServices";
+import * as CategoryServices from "~/apiServices/categoryServices";
+import * as ReportServices from "~/apiServices/reportServices";
+import { ToastContext } from "~/components/ToastContext";
 
 function HomePage() {
-    const toastContext = useContext(ToastContext);
-    const [listSale, setListSale] = useState([])
-    const [listNew, setListNew] = useState([])
-    const [listTop, setListTop] = useState([])
-    const [listJustForYou, setListJustForYou] = useState([])
-    const [listCategories, setListCategories] = useState([])
-    const [pendingSale, setPendingSale] = useState(false)
-    const [pendingNew, setPendingNew] = useState(false)
-    const [pendJustForYou, setPendingJustForYou] = useState(false)
-    const createObjectQuery = async (
-        page,
-        limit,
-        sortBy,
-        orderBy,
-        search,
-        brand,
-        category,
-        classify,
-        price
-    ) => {
-        return {
-            limit,
-            page,
-            ...(orderBy && { orderBy }),
-            ...(sortBy && { sortBy }),
-            ...(search && { search }),
-            ...(brand && { brand }),
-            ...(category && { category }),
-            ...(classify && { classify }),
-            ...(price && { price }),
-        };
-    }
-    const getCate = async () => {
-        const response = await CategoryServices.getAllCategorys(
-        )
-            .catch((error) => {
-                if (error.response) {
-                    console.log(error.response.data);
-                    console.log(error.response.status);
-                    console.log(error.response.headers);
-                } else if (error.request) {
-                    console.log(error.request);
-                } else {
-                    console.log('Error', error.message);
-                }
-                console.log(error.config);
-            });
-
-        if (response) {
-            console.log(response.data);
-            setListCategories(response.data);
-        }
+  const toastContext = useContext(ToastContext);
+  const [listSale, setListSale] = useState([]);
+  const [listNew, setListNew] = useState([]);
+  const [listTop, setListTop] = useState([]);
+  const [listJustForYou, setListJustForYou] = useState([]);
+  const [listCategories, setListCategories] = useState([]);
+  const [pendingSale, setPendingSale] = useState(false);
+  const [pendingNew, setPendingNew] = useState(false);
+  const [pendJustForYou, setPendingJustForYou] = useState(false);
+  const createObjectQuery = async (
+    page,
+    limit,
+    sortBy,
+    orderBy,
+    search,
+    brand,
+    category,
+    classify,
+    price
+  ) => {
+    return {
+      limit,
+      page,
+      ...(orderBy && { orderBy }),
+      ...(sortBy && { sortBy }),
+      ...(search && { search }),
+      ...(brand && { brand }),
+      ...(category && { category }),
+      ...(classify && { classify }),
+      ...(price && { price }),
     };
-    const getList = async (obj, setList, setPending) => {
-        setPending(true);
-        const response = await ProductServices.getAllProducts(obj)
-            .catch((error) => {
-                setPending(false);
+  };
+  const getCate = async () => {
+    const response = await CategoryServices.getAllCategorys().catch((error) => {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
 
-                if (error?.response?.status === 404) {
-                    setList([]);
-                } else {
-                    toastContext.notify('error', 'Có lỗi xảy ra');
-                }
-            });
-
-        if (response) {
-            console.log(response);
-            setPending(false);
-            setList(response.data);
-
-        }
+    if (response) {
+      console.log(response.data);
+      setListCategories(response.data);
     }
-    useEffect(() => {
-        const fetch = async () => {
-            getList(
-                await createObjectQuery(
-                    1,
-                    12,
-                    'discount',
-                    'desc',
+  };
+  const getList = async (obj, setList, setPending) => {
+    setPending(true);
+    const response = await ProductServices.getAllProducts(obj).catch(
+      (error) => {
+        setPending(false);
 
-                ), setListSale, setPendingSale);
-            getList(
-                await createObjectQuery(
-                    1,
-                    12,
-                ), setListNew, setPendingNew);
-            getList(
-                await createObjectQuery(
-                    1,
-                    20,
-                ), setListJustForYou, setPendingJustForYou);
-            const now = new Date();
-            const result = await ReportServices.GetTopProduct({
-                month: now.getMonth() + 1,
-                year: now.getFullYear(),
-            })
-                .catch((error) => {
-                    if (error?.response?.status === 404) {
-                        setListTop([]);
-                    } else {
-                        toastContext.notify('error', 'Có lỗi xảy ra');
-                    }
-                });
-            if (result) {
-                console.log(result);
-                setListTop(result.data)
-            }
-            getCate()
+        if (error?.response?.status === 404) {
+          setList([]);
+        } else {
+          toastContext.notify("error", "Có lỗi xảy ra");
         }
-
-        fetch();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    return (
-        <div className='select-none'>
-            <ProductCarousel title={'Siêu khuyến mãi'} listProduct={listSale} path='/collection/desc&discount' />
-            <Categories list={listCategories} />
-            <ProductCarousel title={'Sản phẩm mới'} listProduct={listNew} path='/collection/desc&creatAt' />
-            <ProductCarousel title={'Mua nhiều nhất'} listProduct={listTop} path='/' />
-            <div className='bg-white m-5 mb-10 p-3 rounded-lg'>
-                <div className='mb-2 ms-3 h-[40px] flex items-center justify-between'>
-                    <div className='font-bold text-[16px]'>Just for you</div>
-                </div>
-                <div className='flex flex-wrap mb-7'>
-                    {
-                        listJustForYou.map((item, index) => (
-                            <div key={index}>
-                                <Product_WC_item product={item} />
-
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
-
-        </div>
+      }
     );
+
+    if (response) {
+      console.log(response);
+      setPending(false);
+      setList(response.data);
+    }
+  };
+  useEffect(() => {
+    const fetch = async () => {
+      getList(
+        await createObjectQuery(1, 12, "discount", "desc"),
+        setListSale,
+        setPendingSale
+      );
+      getList(await createObjectQuery(1, 12), setListNew, setPendingNew);
+      getList(
+        await createObjectQuery(1, 20),
+        setListJustForYou,
+        setPendingJustForYou
+      );
+      const now = new Date();
+      const result = await ReportServices.GetTopProduct({
+        month: now.getMonth() + 1,
+        year: now.getFullYear(),
+      }).catch((error) => {
+        if (error?.response?.status === 404) {
+          setListTop([]);
+        } else {
+          toastContext.notify("error", "Có lỗi xảy ra");
+        }
+      });
+      if (result) {
+        console.log(result);
+        setListTop(result.data);
+      }
+      getCate();
+    };
+
+    fetch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="select-none">
+      <ProductCarousel
+        title={"Siêu khuyến mãi"}
+        listProduct={listSale}
+        path="/collection/desc&discount"
+      />
+      <Categories list={listCategories} />
+      <ProductCarousel
+        title={"Sản phẩm mới"}
+        listProduct={listNew}
+        path="/collection/desc&creatAt"
+      />
+      <ProductCarousel
+        title={"Mua nhiều nhất"}
+        listProduct={listTop}
+        path="/"
+      />
+      <div className="bg-white m-5 mb-10 p-3 rounded-lg">
+        <div className="mb-2 ms-3 h-[40px] flex items-center justify-between">
+          <div className="font-bold text-[16px]">Just for you</div>
+        </div>
+        <div className="flex flex-wrap mb-7">
+          {listJustForYou.map((item, index) => (
+            <div key={index}>
+              <Product_WC_item product={item} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
